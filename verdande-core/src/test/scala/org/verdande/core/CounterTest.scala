df865ddf223db6c45db00890b4b525756d6372fd
Lukasz Dubiel <bambucha14@gmail.com>
2018-12-13 19:33:12 +0100

    Add test for starting with zero.
    Refactor test to use single trait-based fixture

--- verdande-core/src/test/scala/org/verdande/core/CounterTest.scala
+++ verdande-core/src/test/scala/org/verdande/core/CounterTest.scala
@@ -5,21 +5,26 @@ import org.scalatest.{FlatSpec, Matchers}
 class CounterTest extends FlatSpec with Matchers {
   behavior of "Counter"
 
-  it should "allow to increment by one" in {
+  trait ExampleCounter {
     val counter = Counter()
+  }
+
+  it should "start from zero" in new ExampleCounter {
+    counter.value shouldEqual 0.0
+  }
+
+  it should "allow to increment by one" in new ExampleCounter {
     counter.inc()
     counter.value shouldEqual 1.0
   }
 
-  it should "allow to increment by more then one" in {
+  it should "allow to increment by more then one" in new ExampleCounter {
     val value  = 10.0
-    val counter = Counter()
     counter.inc(value)
     counter.value shouldEqual value
   }
 
-  it should "throw exception on negative increment" in {
-    val counter = Counter()
+  it should "throw exception on negative increment" in new ExampleCounter {
     intercept[IllegalArgumentException] {
       counter.inc(-1.0)
     }
